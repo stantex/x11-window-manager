e8c8aebe83f4eae723eda1196eaff4002198ef27
Michael Stanton <michael@stantexinc.com>
2019-05-15 15:58:39 -0700

    Changes

--- start.sh
+++ start.sh
@@ -14,4 +14,16 @@ do
 	sleep 30
 done
 
+# CUPS
+# Tell the container that DBUS should report to Host OS
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
+# Set rules for connection only through the public device URL
+iptables -A INPUT -i resin-vpn -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -i resin-vpn -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j REJECT
+
+cupsd -f
+
+# BROWSER
 chromium-browser
